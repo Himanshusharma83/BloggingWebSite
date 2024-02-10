c7831026e2788e43e7d9d178b487ffcec3b857da
Himanshusharma83 <hs9450331@gmail.com>
2024-02-10 11:18:10 +0530

    Signup page ui

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
-import Header from "./pages/Header"
+import Header from "./components/Header"
 function App() {
  
 
@@ -16,8 +16,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/projects" element = {<Projects/>} />
     </Routes>
