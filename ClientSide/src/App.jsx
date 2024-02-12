1f23f144bb1672411f04aa37af57b9c5749ed604
Himanshusharma83 <hs9450331@gmail.com>
2024-02-12 21:03:38 +0530

    dashboard private route

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import PrivateRoute from "./components/privateRoute"
 function App() {
  
 
@@ -19,7 +20,10 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route element={<PrivateRoute />}>
+
       <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="/projects" element = {<Projects/>} />
     </Routes>
    <Footer />
